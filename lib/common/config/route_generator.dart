5fb4b55a109c4cdafea8e1a757ee65f9ab0dbb02
danqwas <daniel_inmaculada@hotmail.com>
2022-10-21 19:28:58 -0500

    core: common bounded context directory
    This directory contains all about the shared configuration in the app.

---
+++ lib/common/config/route_generator.dart
@@ -0,0 +1,49 @@
+import 'package:easy_story_flutter/iam/presentation/sign_in/login_screen_detail.dart';
+import 'package:easy_story_flutter/publishing/writing/presentation/screens/write_publishing_screen_detail.dart';
+import 'package:flutter/material.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    // Getting arguments passed in while calling Navigator.pushNamed
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => LoginScreenDetail());
+      case '/login':
+        // Validation of correct data type
+        if (args is String) {
+          return MaterialPageRoute(
+            builder: (_) => LoginScreenDetail(),
+          );
+        }
+        // If args is not of the correct type, return an error page.
+        // You can also throw an exception while in development.
+        return _errorRoute();
+      case '/publishing-create':
+        if (args is String) {
+          return MaterialPageRoute(
+            builder: (_) => WritePublishingScreenDetail(),
+          );
+        }
+
+        return _errorRoute();
+      default:
+        // If there is no such named route in the switch statement, e.g. /third
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Error'),
+        ),
+        body: Center(
+          child: Text('ERROR'),
+        ),
+      );
+    });
+  }
+}
---
+++ lib/common/exceptions/server_exceptions.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+
+class ServerException implements Equatable {
+  @override
+  List<Object> get props => [];
+
+  call() {
+    return null;
+  }
+
+  @override
+  bool? get stringify => null;
+}
