c6a845a0ed142097dd1fd7b4ac8e7f093668cff6
danqwas <daniel_inmaculada@hotmail.com>
2022-11-18 04:26:07 -0500

    feat: update loading circle indicator in bounded context publishing

--- lib/publishing/presentation/publishing_create_edit_posts/screens/write_publishing_screen_detail.dart
+++ lib/publishing/presentation/publishing_create_edit_posts/screens/write_publishing_screen_detail.dart
@@ -25,7 +25,7 @@ class _WritePublishingScreenDetailState
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: SignInBackgroundImage(),
+      decoration: PublishingBackgroundImage(),
       child: Scaffold(
         appBar: AppBar(
           title: Text("Create your own Stories"),
--- lib/publishing/presentation/publishing_lists_posts/screens/publishing_user_posts.dart
+++ lib/publishing/presentation/publishing_lists_posts/screens/publishing_user_posts.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import '../../../../common/widgets/drawer_widget.dart';
 import '../../../../common/widgets/widgets.dart';
 import '../../../../iam/presentation/sign_in/widgets/sign_in_background_image.dart';
+import '../../publishing_create_edit_posts/widgets/publishing_background.dart';
 
 class PublishingUserPosts extends StatefulWidget {
   const PublishingUserPosts({Key? key}) : super(key: key);
@@ -30,42 +31,58 @@ class _PublishingUserPostsState extends State<PublishingUserPosts> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color.fromARGB(255, 177, 116, 164),
-        onPressed: () {
-          Navigator.pushNamed(context, '/write-publishing');
-        },
-        child: Icon(
-          Icons.add,
+    return Container(
+      decoration: PublishingBackgroundImage(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.pink,
+          onPressed: () {
+            Navigator.pushNamed(context, '/write-publishing');
+          },
+          child: Icon(
+            Icons.add,
+          ),
         ),
-      ),
-      body: Container(
-        decoration: SignInBackgroundImage(),
-        child: FutureBuilder<List<WriteModel>>(
-            future: posts,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return PublishingItem(
-                        context,
-                        snapshot.data![index].title.toString(),
-                        snapshot.data![index].description.toString(),
-                        snapshot.data![index].content.toString(),
-                        snapshot.data![index].hashtags.toString(),
-                        snapshot.data![index].image.toString(),
-                        snapshot.data![index].id.toString(),
-                        snapshot.data![index].slug.toString());
-                  },
-                );
-              } else if (snapshot.hasError) {
-                return Text(snapshot.error.toString());
-              } else {
-                return CircularProgressIndicator();
-              }
-            }),
+        body: Container(
+          child: FutureBuilder<List<WriteModel>>(
+              future: posts,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return PublishingItem(
+                          context,
+                          snapshot.data![index].title.toString(),
+                          snapshot.data![index].description.toString(),
+                          snapshot.data![index].content.toString(),
+                          snapshot.data![index].hashtags.toString(),
+                          snapshot.data![index].image.toString(),
+                          snapshot.data![index].id.toString(),
+                          snapshot.data![index].slug.toString());
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Text(snapshot.error.toString());
+                } else {
+                  return Container(
+                    decoration: PublishingBackgroundImage(),
+                    width: MediaQuery.of(context).size.width * 1,
+                    height: MediaQuery.of(context).size.height * 1,
+                    child: Center(
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: CircularProgressIndicator(
+                          backgroundColor: Colors.purple,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              }),
+        ),
       ),
     );
   }
